bf08018f93e2bb6a6b752ed0b2cf76de086504de
mgp <mgp@edidding.com.cn>
2017-02-03 11:39:10 +0800

    调整 部分

--- provider/src/main/java/com/varela/rpc/filter/AuthorityFilter.java
+++ provider/src/main/java/com/varela/rpc/filter/AuthorityFilter.java
@@ -17,8 +17,21 @@ public class AuthorityFilter implements Filter {
 
 
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        /**
+         * TODO 服务方 消费方判断
+         *
+         RpcContext.getContext().isConsumerSide();
+         RpcContext.getContext().isProviderSide();
+         */
+
+        //获取调用方IP地址
         String clientIp = RpcContext.getContext().getRemoteHost();
         logger.debug("访问ip为:" + clientIp);
+
+        //获取当前服务配置信息
+        String application = RpcContext.getContext().getUrl().getParameter("application");
+
+
         return invoker.invoke(invocation);
     }
 }
